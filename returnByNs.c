1c632f179dfbdff0adc933156aac2c917486247a
User <Willspri@gmail.com>
2025-02-09 09:35:58 +1300

    remove spaces

--- returnByNs.c
+++ returnByNs.c
@@ -1,11 +1,27 @@
+
 #include <stdio.h>
 
 int main() {
-    char str[] = "Sample string of various letters for testing"
+    char str[] = "Sample string of various letters for testing";
+    //initialise no space array
+    char result[100];
+    //initialise j to 0
+    int j = 0;
 
-    for (int i = 3; str[i] != '\0'; i += 4) {
-        printf("%c ", str[i]);
+    //loop to remove spaces
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] != ' ') {
+            result[j++] = str[i];
+        }
     }
+    result[j] = '\0';
+
+    //changed str to result
+    for (int i = 3; result[i] != '\0'; i += 4) {
+        printf("%c ", result[i]);
+
+    }
+
 
     return 0;
 }
